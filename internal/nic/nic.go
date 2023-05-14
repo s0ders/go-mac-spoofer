fbe3b86c7493cfcbbe14a42a2fe48038e054c4b8
s0ders <39492740+s0ders@users.noreply.github.com>
2023-05-14 22:17:55 +0300

    feat: added 'list' command and Cobra CLI

--- internal/nic/nic.go
+++ internal/nic/nic.go
@@ -2,7 +2,8 @@
 package nic
 
 import (
-	"fmt"
+	"log"
+	"os"
 	"os/exec"
 	"reflect"
 	"regexp"
@@ -13,25 +14,33 @@ var (
 	PATH_TO_AIRPORT = "/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport"
 )
 
-// ListNICs() returns a slice with the name of each available NICs.
-func ListNICs() ([][]byte, error) {
+type NIC struct {
+	Name    []byte
+	Address []byte
+}
+
+// List() returns a slice with the name of each available NICs.
+func List() ([]NIC, error) {
 	platform := runtime.GOOS
 
-	nics := make([][]byte, 0)
+	nics := make([]NIC, 0)
 
 	switch platform {
 	case "darwin":
-		darwinNICRegex := regexp.MustCompile(`Device: ([a-z0-9]+)`)
+		NICNameRegex := regexp.MustCompile(`Device: ([a-z0-9]+)`)
+		NICAddressRegex := regexp.MustCompile(`Ethernet Address: ([a-z0-9]{2}\:[a-z0-9]{2}\:[a-z0-9]{2}\:[a-z0-9]{2}\:[a-z0-9]{2}\:[a-z0-9]{2})`)
 
 		cmd, err := exec.Command("networksetup", "-listallhardwareports").Output()
 
 		if err != nil {
-			return [][]byte{}, err
+			return []NIC{}, err
 		}
 
-		matches := darwinNICRegex.FindAllSubmatch(cmd, -1)
-		for _, match := range matches {
-			nics = append(nics, match[1])
+		nameMatches := NICNameRegex.FindAllSubmatch(cmd, -1)
+		addressMatches := NICAddressRegex.FindAllSubmatch(cmd, -1)
+
+		for i := range nameMatches {
+			nics = append(nics, NIC{nameMatches[i][1], addressMatches[i][1]})
 		}
 	}
 
@@ -40,7 +49,7 @@ func ListNICs() ([][]byte, error) {
 
 // Exists checks if a given NIC exists on the system.
 func Exists(nicName []byte) (bool, error) {
-	existing, err := ListNICs()
+	existing, err := List()
 
 	if err != nil {
 		return false, err
@@ -62,26 +71,29 @@ func ChangeMAC(nicName, newMac []byte) error {
 	switch platform {
 	case "darwin":
 
+		if os.Geteuid() != 0 {
+			log.Fatalf("Are you running this program as root ?")
+		}
+
 		// Disassociate from wi-fi network without turning off wi-fi or the device
 		_, err := exec.Command(PATH_TO_AIRPORT, "-z").Output()
 
 		if err != nil {
-			return err
+			log.Fatalf("error: %s", err.Error())
 		}
 
 		// Changing MAC address
-		ifconfigCmd := fmt.Sprintf("ifconfig %s ether %s", string(nicName), string(newMac))
-		_, err = exec.Command(ifconfigCmd).Output()
+		_, err = exec.Command("ifconfig", string(nicName), "ether", string(newMac)).Output()
 
 		if err != nil {
-			return err
+			log.Fatalf("error: %s", err.Error())
 		}
 
 		// Restart airport on device to reassociate with known networks
 		_, err = exec.Command("networksetup", "-setairportpower", string(nicName), "off").Output()
 
 		if err != nil {
-			return err
+			log.Fatalf("error: %s", err.Error())
 		}
 
 		_, err = exec.Command("networksetup", "-setairportpower", string(nicName), "on").Output()
